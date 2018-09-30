86fd599416220da55c1d25e8aa5912679b19e236
dmajumder4292 <33713432+dmajumder4292@users.noreply.github.com>
2018-09-30 20:52:59 +0530

    Update provider

--- ethereum/createWeb3.js
+++ ethereum/createWeb3.js
@@ -1,11 +1,16 @@
 const Web3 = require('web3');
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var provider = function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/b13e328e12fa4a1bb93c4097b7241e6e")
+      };
 
 let web3 = null;
 module.exports = {
     getWeb3Instance : function(){
         if (web3 === null) {
             web3 = new Web3();
-            web3.setProvider(new Web3.providers.HttpProvider('https://ropsten.infura.io/b13e328e12fa4a1bb93c4097b7241e6e'));
+            web3.setProvider(provider);
         } 
         return web3;
     }
