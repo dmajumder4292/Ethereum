be2062c9be76297a7fb5363a10baec0f593653e8
dmajumder4292 <33713432+dmajumder4292@users.noreply.github.com>
2018-09-30 17:40:54 +0530

    change HttpProvider for Ropsten

--- ethereum/createWeb3.js
+++ ethereum/createWeb3.js
@@ -5,7 +5,7 @@ module.exports = {
     getWeb3Instance : function(){
         if (web3 === null) {
             web3 = new Web3();
-            web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
+            web3.setProvider(new Web3.providers.HttpProvider('https://ropsten.infura.io/b13e328e12fa4a1bb93c4097b7241e6e'));
         } 
         return web3;
     }
